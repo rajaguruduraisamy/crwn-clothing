c54e3590ef43e3f2675f1e37df4e62ef82bd646e
Rajaguru Duraisamy <rajaguruduraisamy@gmail.com>
2021-09-12 01:12:14 -0700

    Add homepage with directory and menu items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import { HomePage } from './pages/homepage/homepage.component';
+
 function App() {
   return (
     <div className="App">
-      Hello Rajaguru!!!
+      <HomePage></HomePage>
     </div>
   );
 }
